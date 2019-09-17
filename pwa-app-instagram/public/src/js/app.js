751b0bd89722da6ba6d4737fd0386c6d45d2d58a
Franco Arratia <franck_ral@hotmail.com>
2019-09-17 15:56:27 -0400

    pwa-service-workders: Reacting to Incoming Events (in SW).

--- pwa-app-instagram/public/src/js/app.js
+++ pwa-app-instagram/public/src/js/app.js
@@ -1,5 +1,6 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
+					//  .register('/sw.js', { scope: '/help' })
 					 .register('/sw.js')
 					 .then(function() {
 						 console.log('Service worker registered!');
