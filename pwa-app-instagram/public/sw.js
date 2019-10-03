1103dfd009c65099586b2cb0d10716aa5d343207
franco-extreme <franck_ral@hotmail.com>
2019-10-02 21:24:01 -0400

    pwa-sw-caching: Implementing Dynamic Cachin

--- pwa-app-instagram/public/sw.js
+++ pwa-app-instagram/public/sw.js
@@ -1,66 +1,65 @@
-
 self.addEventListener('install', function(event) {
-	console.log('[Service Worker] Installing Service Worker ...', event);
+    console.log('[Service Worker] Installing Service Worker ...', event);
 
-	// When we talk about cache API, we can simply have a look on the APPLICATION
-	// tab in developer tools. 
-	// We have only one per given page, per given domain even, there we can open
-	// multiple sub-caches. And this is what we can do with open() method 
-	// caches.open();
-	event.waitUntil(
-		caches.open('static')
-			  .then(function(cache) {
-          console.log('[Service Worker] Precaching App Shell');
-          // Something to take into account, even with those 2 files cached, it will not work.
-          // You have to store the exact requests you are about to make, keep in mind we are
-          // storing request, reponse key-value pairs. Load server/ is not the same like server/index.html
-          // So we also need to cache the / request.
-          // cache.add('/');
-          // cache.add('/index.html');
-          // cache.add('/src/js/app.js');
+    // When we talk about cache API, we can simply have a look on the APPLICATION
+    // tab in developer tools. 
+    // We have only one per given page, per given domain even, there we can open
+    // multiple sub-caches. And this is what we can do with open() method 
+    // caches.open();
+    event.waitUntil(
+        caches.open('static')
+        .then(function(cache) {
+            console.log('[Service Worker] Precaching App Shell');
+            // Something to take into account, even with those 2 files cached, it will not work.
+            // You have to store the exact requests you are about to make, keep in mind we are
+            // storing request, reponse key-value pairs. Load server/ is not the same like server/index.html
+            // So we also need to cache the / request.
+            // cache.add('/');
+            // cache.add('/index.html');
+            // cache.add('/src/js/app.js');
 
-          cache.add('/');
-          cache.add('/index.html');
-          cache.add('/src/js/app.js');
-          cache.add('/src/js/feed.js');
-          cache.add('/src/js/promise.js');
-          cache.add('/src/js/material.min.js');
-          cache.add('/src/css/app.css');
-          cache.add('/src/css/feed.css');
-          cache.add('/src/images/main-image.jpg');
-          cache.add('https://fonts.googleapis.com/css?family=Roboto:400,700');
-          cache.add('https://fonts.googleapis.com/icon?family=Material+Icons');
-          cache.add('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css');
+            cache.add('/');
+            cache.add('/index.html');
+            cache.add('/src/js/app.js');
+            cache.add('/src/js/feed.js');
+            cache.add('/src/js/promise.js');
+            cache.add('/src/js/material.min.js');
+            cache.add('/src/css/app.css');
+            cache.add('/src/css/feed.css');
+            cache.add('/src/images/main-image.jpg');
+            cache.add('https://fonts.googleapis.com/css?family=Roboto:400,700');
+            cache.add('https://fonts.googleapis.com/icon?family=Material+Icons');
+            cache.add('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css');
 
 
-          // promise and fetch: Does it make sense to store these polyfills in our cache?
-          // In my opinion it does not, because we use these polyfills for older browsers. Now these browsers won't
-          // support service workers anyways, so there is no value in storing these files in the cache. If we store
-          // them there, we only load them for browsers which do not need them, so we can also remove them from there.
-          // Now from a performance perspective, storing them in the cache might still be worth it because even modern
-          // browsers have to load these files because we simply import them in the HTML file.
-          // cache.addAll([
-          //   '/',
-          //   '/index.html',
-          //   '/src/js/app.js',
-          //   '/src/js/feed.js',
-          //   '/src/js/promise.js',
-          //   '/src/js/fetch.js',
-          //   '/src/js/material.min.js',
-          //   '/src/css/app.css',
-          //   '/src/css/feed.css',
-          //   '/src/images/main-image.jpg',
-          //   'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          //   'https://fonts.googleapis.com/icon?family=Material+Icons',
-          //   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-          // ]);
-			  })
-	);
+            // promise and fetch: Does it make sense to store these polyfills in our cache?
+            // In my opinion it does not, because we use these polyfills for older browsers. Now these browsers won't
+            // support service workers anyways, so there is no value in storing these files in the cache. If we store
+            // them there, we only load them for browsers which do not need them, so we can also remove them from there.
+            // Now from a performance perspective, storing them in the cache might still be worth it because even modern
+            // browsers have to load these files because we simply import them in the HTML file.
+            // cache.addAll([
+            //   '/',
+            //   '/index.html',
+            //   '/src/js/app.js',
+            //   '/src/js/feed.js',
+            //   '/src/js/promise.js',
+            //   '/src/js/fetch.js',
+            //   '/src/js/material.min.js',
+            //   '/src/css/app.css',
+            //   '/src/css/feed.css',
+            //   '/src/images/main-image.jpg',
+            //   'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            //   'https://fonts.googleapis.com/icon?family=Material+Icons',
+            //   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            // ]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
-	console.log('[Service Worker] Activating Service Worker ...', event);
-	return self.clients.claim();
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
 });
 
 // Until here, when you refresh the page, you will see only the INSTALL listener has been called. So the obvious one is the activate log is missing.
@@ -75,22 +74,25 @@ self.addEventListener('activate', function(event) {
 // breaking changes, might break the running page. Therefore the way to activate a new version is to close the existing tab and reopen it so that you have
 
 self.addEventListener('fetch', function(event) {
-	console.log('[Service Worker] Fetching something ...', event);
-	// event.respondWith(null);
-	// event.respondWith(fetch(event.request));
+    console.log('[Service Worker] Fetching something ...', event);
+    // event.respondWith(null);
+    // event.respondWith(fetch(event.request));
 
-	event.respondWith(
-		caches.match(event.request)
-			  .then(function(response) {
-				  if (response) {
-					  return response;
-				  } else {
-            return fetch(event.request);
-          }
-			  })
-	);
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request)
+                    .then(function(res) {
+                        return caches.open('dynamic')
+                            .then(function(cache) {
+                                cache.put(event.request.url, res);
+                                return res;
+                            });
+                    });
+            }
+        })
+    );
 });
-
-
-
-
